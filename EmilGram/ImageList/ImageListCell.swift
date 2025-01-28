3b7f3536a40c6e22cbe10541de03d9acb163232d
EmilAxme <emil.azmedov@mail.ru>
2025-01-28 15:01:42 +0300

    проделана работа с передачей контента в ячейки, обновлен алгоритм высоты ячеек

--- EmilGram/ImageList/ImageListCell.swift
+++ EmilGram/ImageList/ImageListCell.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-    
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
     static let reuseIdentifier = "ImageListCell"
     
     
