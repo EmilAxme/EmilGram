4640846c1d145403fb287c77446c2228996e7ab6
EmilAxme <emil.azmedov@mail.ru>
2025-02-01 11:29:34 +0300

    Добавлены UI на экран Profile, добавлены constrains

---
+++ EmilGram/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  EmilGram
+//
+//  Created by Emil on 31.01.2025.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController{
+    override func viewDidLoad() {
+    }
+}
