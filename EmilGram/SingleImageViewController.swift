621b50b5f8b8bd4ac29cd2e0dcefc426be3f01d6
EmilAxme <emil.azmedov@mail.ru>
2025-02-02 22:29:47 +0300

    Изменен способ перехода на Present, добавлен BackButton

--- EmilGram/SingleImageViewController.swift
+++ EmilGram/SingleImageViewController.swift
@@ -3,6 +3,9 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image: UIImage?
     
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
     @IBOutlet private var singleImage: UIImageView!{
         didSet{
             guard isViewLoaded else { return }
