2f1b2a3ca0234e3efb5db88dee544d02e02e336b
EmilAxme <emil.azmedov@mail.ru>
2025-02-17 17:45:23 +0300

    добавлен loadAuthView(), позволяющий октрывать страницу с аутентификацией

--- EmilGram/Auth/WebViewViewController.swift
+++ EmilGram/Auth/WebViewViewController.swift
@@ -2,22 +2,23 @@ import UIKit
 import WebKit
 
 final class WebViewViewController: UIViewController {
+    //MARK: - View's
     lazy var webView: WKWebView = {
         var WKWebView = WKWebView()
         view.addToView(WKWebView)
         WKWebView.backgroundColor = .white
         return WKWebView
-        
-    
     }()
     
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         setupUI()
+        loadAuthView()
         super.viewDidLoad()
         
     }
-    
-    func setupUI() {
+    //MARK: - Private function's
+    private func setupUI() {
         NSLayoutConstraint.activate([
             webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -26,4 +27,24 @@ final class WebViewViewController: UIViewController {
         ])
     }
     
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else { return }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        
+        guard let url = urlComponents.url else { return }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+
+    //MARK: - ENUM
+enum WebViewConstants {
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
 }
